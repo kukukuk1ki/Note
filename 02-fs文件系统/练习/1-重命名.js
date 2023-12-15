8eac9390950962990febe3b132c58f8290c9f574
xkxhx <1395038773@qq.com>
2023-12-15 11:50:54 +0800

    fs文件系统-批量重命名

--- 02-fs文件系统/练习/1-重命名.js
+++ 02-fs文件系统/练习/1-重命名.js
@@ -3,21 +3,30 @@ const fs = require('fs')
 const files = fs.readdirSync('./code')
 // console.log(files)
 
-// 不足10的加上0
-files.forEach(item => {
+// 不足10的加上0，并且序号不连续的也改为连续
+files.forEach((item, index) => {
   let data = item.split('-')
   let num = data[0]
   let name = data[1]
   // console.log(num, name)
   if (Number(num) < 10) {
-    num = '0' + num
-    // console.log(num)
+    if (num.length === 1) {
+      num = '0' + (index + 1)
+      // console.log(num)
+    }
+    num = '0' + (index + 1)
+    // console.log('888  ', num)
+  } else {
+    num = index + 1
+    if (num < 10) {
+      num = '0' + (index + 1)
+    }
+    // console.log('777  ', num)
   }
   let newName = num + '-' + name
-  // console.log(newName)
+  console.log(newName)
   fs.renameSync(`./code/${item}`, `./code/${newName}`)
 })
-
 
 // 把0去掉
 // files.forEach(item => {
@@ -32,4 +41,3 @@ files.forEach(item => {
 //   // console.log(newName)
 //   fs.renameSync(`./code/${item}`, `./code/${newName}`)
 // })
-
