a51fdd109dad63c78c929760371bfb9485daf2b3
xkxhx <1395038773@qq.com>
2024-04-29 15:06:49 +0800

    HTTP

--- 04-HTTP/05-提取HTTP报文中URL的路径与查询字符串.js
+++ 04-HTTP/05-提取HTTP报文中URL的路径与查询字符串.js
@@ -3,19 +3,21 @@ const http = require('http')
 // 2.导入 url 模块
 const url = require('url')
 
-
 // 创建服务对象
 const server = http.createServer((request, response) => {
+  // http://127.0.0.1:9000/search?keyword=666&pwd=123
+
   // 2.解析 requests.url
   // console.log(request.url)
   let res = url.parse(request.url, true)
   // console.log(res)
   // 路径
   let path = res.pathname
-  // console.log(path)
+  console.log('path: ', path)
   // 查询字符串
   let keyword = res.query.keyword
-  console.log(keyword)
+  let pwd = res.query.pwd
+  console.log('query: ', res.query)
 
   response.end('url')
 });
--- 04-HTTP/06-提取HTTP报文中URL的路径与查询字符串.js
+++ 04-HTTP/06-提取HTTP报文中URL的路径与查询字符串.js
@@ -1,13 +1,10 @@
 // 导入 http 模块
 const http = require('http')
-// 2.导入 url 模块
-const url = require('url')
-
 
 // 创建服务对象
 const server = http.createServer((request, response) => {
   // http://127.0.0.1:9000/search?keyword=666&pwd=123
-  
+
   // 实例化 URL 对象
   // let url = new URL('/search?a=100&b=200', 'http://127.0.0.1')
   let url = new URL(request.url, 'http://127.0.0.1')
@@ -15,13 +12,13 @@ const server = http.createServer((request, response) => {
   // 输出路径
   // console.log(url.pathname)
   // 输出 keyword 查询字符串
-  console.log(1, url.searchParams.get('keyword'))
-  console.log(2, url.searchParams.get('pwd'))
+  console.log(url.searchParams.get('keyword'))
+  console.log(url.searchParams.get('pwd'))
 
   response.end('url new')
-});
+})
 
 // 监听端口, 启动服务
 server.listen(9000, () => {
   console.log('服务已经启动....')
-});
+})
