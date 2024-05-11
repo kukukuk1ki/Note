40fade5cad8993e46841d117db8509a8a1a7db38
xkxhx <1395038773@qq.com>
2024-05-11 16:26:40 +0800

    npm包更新

--- 06-包管理工具/03-发布一个包/index.js
+++ 06-包管理工具/03-发布一个包/index.js
@@ -1,7 +1,14 @@
+// 加法运算
 function add(num1, num2) {
   return num1 + num2
 }
 
+// 减法运算
+function sub(num1, num2) {
+  return num1 - num2
+}
+
 module.exports = {
-  add
+  add,
+  sub
 }
---
+++ 06-包管理工具/03-发布一个包/test.js
@@ -0,0 +1,5 @@
+// 测试功能是否可用
+
+const math = require('./index')
+console.log(math.add(5, 2))
+console.log(math.sub(5, 2))
