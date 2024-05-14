21bdd052c70194f1c2903c6edc8bfede32b837b2
xkxhx <1395038773@qq.com>
2024-05-14 10:37:34 +0800

    express-查看文件上传报文

---
+++ 07-express/16-文件上传/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+// 显示网页的(表单)
+router.get('/portrait', (req, res, next) => {
+  res.render('portrait')
+})
+
+// 处理文件上传
+router.post('/portrait', (req, res, next) => {
+  res.send('成功')
+})
+
+module.exports = router;
