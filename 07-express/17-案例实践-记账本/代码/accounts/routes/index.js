07d3bd77b694022b3b804c5523aa9c61a555a5df
xkxhx <1395038773@qq.com>
2024-05-14 14:26:06 +0800

    案例实践-保存账单信息

--- 07-express/17-案例实践-记账本/代码/accounts/routes/index.js
+++ 07-express/17-案例实践-记账本/代码/accounts/routes/index.js
@@ -1,5 +1,14 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+// 导入lowdb
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync(__dirname + '/../data/db.json')
+//获取 db 对象
+const db = low(adapter)
+// 导入shortid
+const shortid = require('shortid')
+
 
 // 记账本列表
 router.get('/accounts', function(req, res, next) {
@@ -16,7 +25,12 @@ router.get('/accounts/create', function(req, res, next) {
 // 新增记录
 router.post('/account', function(req, res, next) {
   // 获取请求体数据
-  console.log(req.body)
+  // console.log(req.body)
+  // 生成id
+  let id = shortid.generate()
+  // 写入文件
+  // db.get('accounts').unshift(req.body).write()
+  db.get('accounts').unshift({id: id, ...req.body}).write()
   res.send('添加记录')
 });
 
