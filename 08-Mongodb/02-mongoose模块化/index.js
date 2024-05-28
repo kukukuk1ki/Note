1d4a90f8082ac340eadf2291510012c5a4044afa
xkxhx <1395038773@qq.com>
2024-05-28 15:48:42 +0800

    mongoose模块化-抽离模型代码

--- 08-Mongodb/02-mongoose模块化/index.js
+++ 08-Mongodb/02-mongoose模块化/index.js
@@ -2,22 +2,12 @@
 const db = require('./db/db')
 // 导入mongoose
 const mongoose = require('mongoose')
-
+// 导入 BookModel
+const BookModel = require('./models/BookModel')
 // 调用函数
 db(() => {
-  // 5.创建文档的结构对象
-  // 设置集合中文档的属性以及属性值的类型
-  let BookSchma = new mongoose.Schema({
-    name: String,
-    author: String,
-    price: Number,
-  })
-
-  // 6.创建模型对象  对文档操作的封装对象
-  let BookModal = mongoose.model('books', BookSchma)
-
   // 7.新增
-  BookModal.create({
+  BookModel.create({
     name: '西游记',
     author: '吴承恩',
     price: 19.9,
