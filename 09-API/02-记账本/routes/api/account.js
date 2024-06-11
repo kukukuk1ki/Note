ae9a5f8a1d515c22d2879d7b4d8dc64b23ef956a
xkxhx <1395038773@qq.com>
2024-06-12 00:17:57 +0800

    记账本接口-获取账单接口&创建账单接口

---
+++ 09-API/02-记账本/routes/api/account.js
@@ -0,0 +1,78 @@
+var express = require('express')
+var router = express.Router()
+// 导入moment
+const moment = require('moment')
+const AccountModal = require('../../models/AccountModel')
+
+// 记账本列表
+router.get('/account', function (req, res, next) {
+  AccountModal.find().sort({ time:-1 }).exec()
+    .then(data => {
+      // 响应成功的提示
+      res.json({
+        // 响应编号
+        code: '0000',
+        // 响应信息
+        msg: '获取成功',
+        // 响应数据
+        data: data
+      })
+    })
+    .catch(err => {
+      res.json({
+        // 响应编号
+        code: '1001',
+        // 响应信息
+        msg: '获取失败~~~',
+        // 响应数据
+        data: null
+      })
+    })
+})
+
+
+// 新增记录
+router.post('/account', function (req, res, next) {
+  // 修改time属性的值
+  req.body.time = new Date(req.body.time)
+  // 插入数据库
+  AccountModal.create({
+    ...req.body,
+  })
+    .then(data => {
+      // 成功提醒
+      res.json({
+        // 响应编号
+        code: '0000',
+        // 响应信息
+        msg: '新增成功',
+        // 响应数据
+        data: data
+      })
+    })
+    .catch(err => {
+      res.json({
+        // 响应编号
+        code: '1001',
+        // 响应信息
+        msg: '新增失败~~~',
+        // 响应数据
+        data: null
+      })
+    })
+})
+
+// 删除记录
+router.get('/account/:id', function (req, res, next) {
+  // 获取 params 的 id 参数
+  let id = req.params.id
+  // 删除
+  AccountModal.deleteOne({ _id: id }).then((data, err) => {
+    if (err) {
+      console.log('删除失败', err)
+    }
+    res.render('success', { msg: '删除成功', url: '/account' })
+  })
+})
+
+module.exports = router
