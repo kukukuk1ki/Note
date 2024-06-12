17abb7f446148b2df797d9a7b86c0ace795b3153
xkxhx <1395038773@qq.com>
2024-06-13 00:30:08 +0800

    会话控制-express删除cookie

--- 10-会话控制/01-cookie和session/01-cookie.js
+++ 10-会话控制/01-cookie和session/01-cookie.js
@@ -6,7 +6,15 @@ const app = express()
 app.get('/set-cookie', (req, res) => {
   // res.cookie('name','zhangsan') // 浏览器关闭后，cookie失效
   res.cookie('name', 'lisi', {maxAge: 20 * 1000})
+  res.cookie('theme', 'green')
   res.send('Hello World!')
+})
+
+// 删除cookie
+app.get('/remove-cookie', (req, res) => {
+  // 调用方法
+  res.clearCookie('name')
+  res.send('删除成功~~~')
 })
 
 // 启动服务
